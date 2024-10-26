e991528b7af806ca38c98b5bf83616a628365d59
Andreas Müller <schnitzeltony@gmail.com>
2024-10-26 21:51:30 +0200

    Add an override ctor on ServerInterface to support network mock
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- resman-lib/server/resman_serverinterface.h
+++ resman-lib/server/resman_serverinterface.h
@@ -2,6 +2,7 @@
 #define H2012_SERVERINTERFACE_H
 
 #include <vtcp_server.h>
+#include <abstracttcpworkerfactory.h>
 #include <QObject>
 #include <QSet>
 
@@ -22,6 +23,7 @@ class ServerInterface : public QObject
     Q_OBJECT
 public:
     explicit ServerInterface(SCPI::SCPIInterface *t_scpiInterface, QObject *t_parent = 0);
+    explicit ServerInterface(SCPI::SCPIInterface *t_scpiInterface, VeinTcp::AbstractTcpWorkerFactoryPtr tcpWorkerFactory, QObject *t_parent = 0);
     ~ServerInterface();
     void start();
 
