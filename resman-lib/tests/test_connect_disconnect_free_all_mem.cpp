b495531703e2338575035a57710c4864073cdd00
Andreas Müller <schnitzeltony@gmail.com>
2025-01-06 19:43:14 +0100

    test_connect_disconnect_free_all_mem: Make use of TcpNetworkFactory
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- resman-lib/tests/test_connect_disconnect_free_all_mem.cpp
+++ resman-lib/tests/test_connect_disconnect_free_all_mem.cpp
@@ -12,7 +12,7 @@ void test_connect_disconnect_free_all_mem::connect()
     ResmanRunFacade resman(VeinTcp::TcpNetworkFactory::create());
     TimeMachineObject::feedEventLoop();
 
-    VeinTcp::TcpPeer rmConnection;
+    VeinTcp::TcpPeer rmConnection(VeinTcp::TcpNetworkFactory::create());
     QSignalSpy connectionSpy(&rmConnection, &VeinTcp::TcpPeer::sigConnectionEstablished);
     rmConnection.startConnection("127.0.0.1", 6312);
     TimeMachineObject::feedEventLoop();
