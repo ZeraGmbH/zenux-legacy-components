4e2a205908d3bded6408714bf08196c79f97b6d2
Andreas Müller <schnitzeltony@gmail.com>
2025-01-21 00:06:29 +0100

    useratan: De-mystify / stop re-inventing by using atan2
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-math/useratan.cpp
+++ zera-math/useratan.cpp
@@ -2,18 +2,8 @@
 
 double userAtan(double im, double re)
 {
-    double arctan;
-
-    if (fabs(re) < 1e-7)
-        arctan = 90.0;
-    else
-        arctan = atan(im/re) * 57.2957795;
-
-    if (re < 0.0)
-        arctan += 180.0;
-
+    double arctan = atan2(im, re) * 360.0 / (2*M_PI);
     if (arctan < 0.0)
         arctan += 360.0;
-
     return arctan;
 }
