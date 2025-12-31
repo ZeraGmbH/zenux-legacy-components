3839ed3badd5ff85622da241a90fbc099b15c20c
Andreas Müller <schnitzeltony@gmail.com>
2025-12-31 01:22:14 +0100

    resman_scpiinterface.cpp: Fix warnings / prettify code
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- resman-lib/scpi/resman_scpiinterface.cpp
+++ resman-lib/scpi/resman_scpiinterface.cpp
@@ -1,15 +1,12 @@
 #include "resman_scpiinterface.h"
 #include "resman_catalog.h"
-#include "resman_delegate.h"
 #include "resourcemanager.h"
 #include "resman_resourcescpiobject.h"
 #include "resman_resource.h"
 #include "resman_resourceidentity.h"
 #include "resman_clientmultiton.h"
-
 #include <scpiobject.h>
 #include <scpicommand.h>
-
 #include <QStringList>
 #include <QDebug>
 
@@ -56,81 +53,64 @@ void SCPIInterface::onScpiTransaction(ResourceServer::IClientMultiton *t_clientM
 {
     Q_ASSERT(t_clientMultiton != nullptr);
 
-    QString answer="";
-    const cSCPICommand command(QString("%1 %2").arg(QString::fromStdString(t_pbSCPICommand.command())).arg(QString::fromStdString(t_pbSCPICommand.parameter())));
+    const cSCPICommand command(QString("%1 %2").
+                               arg(QString::fromStdString(t_pbSCPICommand.command()),
+                                   QString::fromStdString(t_pbSCPICommand.parameter())));
     ScpiObjectPtr tmpObject = m_scpiInstance.getSCPIObject(command); //check which scpi node is triggered
 
     if(tmpObject!=nullptr) {
         bool retVal=false;
         /// @todo remove debug code
-        answer=QString("Command: %2 Params(%3): %4;").arg(command.getCommand()).arg(command.getParamCount()).arg(command.getParamList().join(';'));
+        QString answer = QString("Command: %2 Params(%3): %4;").arg(command.getCommand()).arg(command.getParamCount()).arg(command.getParamList().join(';'));
         qDebug() << QString("Client: '%1'").arg(t_clientMultiton->getName()) << "\n " <<
                     QString("Command: '%1'").arg(command.getCommand()) << "\n " <<
                     QString("Params: '%1'").arg(command.getParamList().join(';'));
 
-        if(tmpObject==m_addResource) //A resource is about to be added
-        {
+        if(tmpObject == m_addResource) //A resource is about to be added
             retVal = scpiAddResource(command, t_clientMultiton, answer);
-        }
-        else if(tmpObject==m_removeResource)
-        {
+        else if(tmpObject == m_removeResource) {
             const QString resourceName = command.getParamList().last();
             const QList<Application::ResourceIdentity *> providedResourcesToRemove = m_resourceManager->getResourceIdentitiesOf<ResourceServer::IClientMultiton *>(t_clientMultiton);
-            for(Application::ResourceIdentity *tmpResourceToRemove : providedResourcesToRemove)
-            {
-                if(resourceName == tmpResourceToRemove->getResource()->getName())
-                {
+            for(Application::ResourceIdentity *tmpResourceToRemove : providedResourcesToRemove) {
+                if(resourceName == tmpResourceToRemove->getResource()->getName()) {
                     qDebug() << "Removing resource: " << tmpResourceToRemove->getResource()->getName() << "on behalf of:" << t_clientMultiton->getName();
                     m_resourceManager->removeResourceIdentity(tmpResourceToRemove);
                     retVal = true;
                 }
             }
         }
-        else if(tmpObject==m_resourceModel) // return XMLized qstandarditem model
-        {
+        else if(tmpObject == m_resourceModel) { // return XMLized qstandarditem model
             answer="";
             m_scpiInstance.exportSCPIModelXML(answer);
             retVal=true;
         }
-        else if(tmpObject==m_resourceProvider) // return IP address and port
-        {
+        else if(tmpObject == m_resourceProvider) { // return IP address and port
             Application::ResourceIdentity *resourceIdentity = m_resourceManager->getResourceIdentityOf<ScpiObjectPtr>(tmpObject);
-            if(resourceIdentity != nullptr)
-            {
+            if(resourceIdentity != nullptr) {
                 answer = QString("%1;%2;").arg(resourceIdentity->getProvider()->getIpAddress()).arg(resourceIdentity->getResource()->getPort());
                 retVal = true;
             }
             else
-            {
-                answer=tr("Resource not found: %1").arg(command.getParam(CommandParams::name));
-            }
+                answer = tr("Resource not found: %1").arg(command.getParam(CommandParams::name));
         }
-        else if(tmpObject==m_catalogType)
-        {
+        else if(tmpObject == m_catalogType) {
             answer=listTypes();
             retVal=true;
         }
-        else // this is a true ScpiObject try the standard routine
-        {
-            if(!tmpObject->executeSCPI(command.getCommand(),answer))
-            {
+        else { // this is a true ScpiObject try the standard routine
+            if(!tmpObject->executeSCPI(command.getCommand(), answer)) {
                 Application::ResourceIdentity *resourceIdentity = m_resourceManager->getResourceIdentityOf<ScpiObjectPtr>(tmpObject);
-                if(resourceIdentity != nullptr)
-                {
-                    if(command.getParam(SetParams::command).toLower()==SetParams::SET_RESOURCE)
-                    {
+                if(resourceIdentity != nullptr) {
+                    if(command.getParam(SetParams::command).toLower() == SetParams::SET_RESOURCE) {
                         retVal = (resourceIdentity->occupyResource(t_clientMultiton, command.getParam(SetParams::amount).toInt()) == true);
-                        if(retVal == false)
-                        {
+                        if(retVal == false) {
 #warning empty scpi answer here
                             //answer=tr("INSERT HERE");
                         }
                     }
-                    else if(command.getParam(FreeParams::command).toLower()==FreeParams::FREE_RESOURCE)
-                    {
+                    else if(command.getParam(FreeParams::command).toLower() == FreeParams::FREE_RESOURCE) {
                         retVal = (resourceIdentity->releaseResource(t_clientMultiton) == true);
-                        if(retVal == false)
-                        {
+                        if(retVal == false) {
 #warning empty scpi answer here
                             //answer=tr("INSERT HERE");
                         }
@@ -138,39 +118,29 @@ void SCPIInterface::onScpiTransaction(ResourceServer::IClientMultiton *t_clientM
                 }
             }
             else
-            {
                 retVal=true;
-            }
         }
 
         if(retVal == true)
-        {
             t_clientMultiton->doSendACK(answer);
-        }
         else
-        {
             t_clientMultiton->doSendNACK(answer);
-        }
     }
     else //tmpObject was not found
-    {
         t_clientMultiton->doSendError(tr("ERROR INVALID SCPI COMMAND"));
-    }
 }
 
 void SCPIInterface::onClientMultitonDisconnected(ResourceServer::IClientMultiton *t_clientMultiton)
 {
     Q_ASSERT(t_clientMultiton != nullptr);
     const QList<Application::ResourceIdentity *> occupiedResourcesToRelease = m_resourceManager->getOccupationsByClient(t_clientMultiton);
-    for(Application::ResourceIdentity *tmpResourceToRelease : occupiedResourcesToRelease)
-    {
+    for(Application::ResourceIdentity *tmpResourceToRelease : occupiedResourcesToRelease) {
         qDebug() << "Releasing resource: " << tmpResourceToRelease->getResource()->getName() << "on behalf of:" << t_clientMultiton->getName();
         tmpResourceToRelease->releaseResource(t_clientMultiton);
     }
 
     const QList<Application::ResourceIdentity *> providedResourcesToRemove = m_resourceManager->getResourceIdentitiesOf<ResourceServer::IClientMultiton *>(t_clientMultiton);
-    for(Application::ResourceIdentity *tmpResourceToRemove : providedResourcesToRemove)
-    {
+    for(Application::ResourceIdentity *tmpResourceToRemove : providedResourcesToRemove) {
         qDebug() << "Removing resource: " << tmpResourceToRemove->getResource()->getName() << "on behalf of:" << t_clientMultiton->getName();
         m_resourceManager->removeResourceIdentity(tmpResourceToRemove);
     }
@@ -181,13 +151,11 @@ bool SCPIInterface::scpiAddResource(cSCPICommand t_command, ResourceServer::ICli
     Q_ASSERT(t_clientMultiton != nullptr);
 
     bool retVal = false;
-    if(t_command.getParamCount()==CommandParams::_paramcount)
-    {
+    if(t_command.getParamCount()==CommandParams::_paramcount) {
         quint16 port=0;
         bool portConvert;
         port=t_command.getParam(CommandParams::port).toUShort(&portConvert);
-        if(portConvert) // check if the provider has a valid TCP/IP port where the resource is located
-        {
+        if(portConvert) { // check if the provider has a valid TCP/IP port where the resource is located
             Application::Resource* newResource = new Application::Resource(t_command.getParam(CommandParams::amount).toUInt(),
                                                                            t_command.getParam(CommandParams::description),
                                                                            t_command.getParam(CommandParams::name),
@@ -198,7 +166,8 @@ bool SCPIInterface::scpiAddResource(cSCPICommand t_command, ResourceServer::ICli
             const QStringList scpiHierarchy { "RESOURCE", resourceType};
             ResourceSCPIObjectPtr newResourceSCPIObject = std::make_shared<ResourceSCPIObject>(newResource->getName(), isQuery);
             CatalogPtr resCatalog = getOrCreateResourceTypeCatalog(resourceType, scpiHierarchy);
-            cSCPICommand tmpCommand(QString("%1:%2").arg(scpiHierarchy.join(':')).arg(newResource->getName()));
+            cSCPICommand tmpCommand(QString("%1:%2").
+                                    arg(scpiHierarchy.join(':'), newResource->getName()));
             Application::ResourceIdentity *resourceIdentity = new Application::ResourceIdentity(newResource, t_clientMultiton, resCatalog, newResourceSCPIObject, tmpCommand);
 
             newResourceSCPIObject->setResourceIdentity(resourceIdentity);
@@ -209,14 +178,10 @@ bool SCPIInterface::scpiAddResource(cSCPICommand t_command, ResourceServer::ICli
             retVal=true;
         }
         else
-        {
             t_answerOut=tr("Invalid parameter %1: expected 16 bit positive unsigned integer").arg(CommandParams::port+1);
-        }
     }
     else
-    {
         t_answerOut=tr("Invalid parameter count: %1 (expected: %2)").arg(t_command.getParamCount()).arg(CommandParams::_paramcount);
-    }
     return retVal;
 }
 
@@ -228,17 +193,13 @@ CatalogPtr SCPIInterface::getOrCreateResourceTypeCatalog(const QString &t_resour
     QHash<QString, std::shared_ptr<Catalog>> resourceCatalogHash = getTypedCatalogHash();
 
     if(resourceCatalogHash.contains(t_resourceType))
-    {
         retVal = resourceCatalogHash.value(t_resourceType);
-    }
-    else //add a new catalog for the type
-    {
+    else { //add a new catalog for the type
         std::shared_ptr<Catalog> newCatalog= std::make_shared<Catalog>(m_resourceManager, "CATALOG", isCmd);
         newCatalog->setCatalogType(t_resourceType);
         m_scpiInstance.insertScpiCmd(t_scpiHierarchy, newCatalog);
         retVal = newCatalog;
     }
-
     return retVal;
 }
 
